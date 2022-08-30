681ed1067f87855d33796c87c2f32cccfbd2fec1
Luiz Seibel <luiz.seibel@alunos.sc.senac.br>
2022-08-29 21:14:55 -0300

    Criação dos objetos

--- src/poo/POO.java
+++ src/poo/POO.java
@@ -15,7 +15,30 @@ public class POO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Carro meuCarro = new Carro();
+        
+        meuCarro.setCor("Preto");
+        meuCarro.setnumPortas(4);
+        meuCarro.setPlaca("ASDA-1232");
+        meuCarro.setTipo("Gol");
+        
+        System.out.println("----------CARRO----------");
+        System.out.println("Cor: "+meuCarro.getCor());
+        System.out.println("N° Portas: "+meuCarro.getnumPortas());
+        System.out.println("Placa: "+meuCarro.getPlaca());
+        System.out.println("Tipo: "+meuCarro.getTipo());        
+        
+        Pessoa aPessoa = new Pessoa();
+        
+        aPessoa.setNome("Mini Barba Negra");
+        aPessoa.setbioTipo("Anão");
+        aPessoa.setcorDoCabelo("Preto");
+        aPessoa.setidade(45);
+        
+        System.out.println("----------Pessoa----------");
+        System.out.println("Nome: "+aPessoa.getNome());
+        System.out.println("Bio Tipo: "+aPessoa.getbioTipo());
+        System.out.println("Cor do Cabelo: "+aPessoa.getcorDoCabelo());
+        System.out.println("Idade: "+aPessoa.getidade());
     }
-    
 }
